cd2c3569ffd142e8b64214d903888f25d51523bf
mahume <michael.a.hume@gmail.com>
2019-10-02 21:01:02 -0600

    Edited some styles on the Navbar

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import { palette } from "../../utils/colorPalette";
 
 export const Wrapper = styled.nav`
   height: 10vh;
   width: 100%;
-  background: gray;
+  background: ${palette.primary};
   padding: 10px;
 
   display: flex;
